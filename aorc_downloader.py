bd9827329e2bebceee1f5b0dbb2b500da498c826
Fitsume Teshome Wolkeba <94723660+Zprivf@users.noreply.github.com>
2024-02-29 12:11:34 -0600

    Update aorc_downloader.py

--- aorc_downloader.py
+++ aorc_downloader.py
@@ -4,20 +4,23 @@ def AORC_downloader (url,output_directory):
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
     try:
-        os.system(f"wget -c --mirror --no-parent --no-check-certificate  {url} -P {output_directory}")
-        print(f"Contents of {url} folder downloaded and saved in {output_directory}")
-    except Exception as e:
-        print("url failed:", e)
-    else:
-        exit() 
-    try:
-        os.system(f"wget -c --no-check-certificate  {url} -P {output_directory}")
-        print(f"File {url} downloaded and saved in {output_directory}")
-    except Exception as e:
-        print("url failed:", e)
-    else:
-        print("url succeeded")
-        exit()  
+        os.system(f"wget -c --mirror --no-parent --no-check-certificate  {url} -P {output_directory}")  
+        print(f"Contents of folder {url} downloaded and saved in {output_directory}")
+    except:    
+        print(" Web folder not working, check and run again")
+        try:
+            os.system(f"wget -c --no-check-certificate  {url} -P {output_directory}")
+            print(f"Contents of file {url} downloaded and saved in {output_directory}")
+        except:
+            print(" file not availible, check file and run again")
+            try:
+                os.system(f"wget -ci --no-check-certificate  {url} -P {output_directory}")
+                print(f"Contents of text {url} downloaded and saved in {output_directory}")
+            except Exception as e:
+                print("text file failed, check file and run again:", e)
+            else:
+                print("Something failed please recheck url and downloadfolder again:")
 
-    os.system(f"wget -ci --no-check-certificate  {url} -P {output_directory}")
-    print(f"Contents of list {url} downloaded and saved in {output_directory}")
+        
+
+        
